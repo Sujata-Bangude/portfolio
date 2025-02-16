6ba63c7db5f479f8eb27faaa4416d4487c5e84d7
Sujata Bangude <sujatabangude@46gmail.com>
2025-02-17 02:07:38 +0530

    Commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function openTab(tabId, btn) {
-    // Remove active class from all buttons
+ 
     document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
 
-    // Hide all tab contents
+   
     document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
 
-    // Show the selected tab content
+   
     document.getElementById(tabId).classList.add('active');
 
-    // Add active class to clicked button
+  
     btn.classList.add('active');
 }
 
@@ -29,3 +29,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+    alert('Form Submitted Successfully!');
+  });
